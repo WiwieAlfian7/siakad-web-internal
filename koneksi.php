185435b45be329ff80577c2d55598e2f51462c6f
WiwieAlfianaAin <wiwiealfianaain@gmail.com>
2026-04-27 04:21:23 +0800

    Menyesuaikan koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,12 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'db_akademik_ibnu');
+    define('DB_USER', 'siakaduser');
+    define('DB_PASS', 'siakad123');
+    define('DB_NAME', 'siakad_unm');
 
-    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)
-    or die ("Koneksi Gagal");
+    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
+    if (!$link) {
+        die("Koneksi database gagal: " . mysqli_connect_error());
+    }
 ?>
